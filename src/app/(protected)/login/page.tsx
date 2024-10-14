73d7a183c872157c7924de39e5e0f4a8b0aeacf2
deshmukh-ajinkya <deshmukhasb@gmail.com>
2024-10-14 21:47:59 +0530

    feat: login api integration

--- src/app/(protected)/login/page.tsx
+++ src/app/(protected)/login/page.tsx
@@ -4,23 +4,39 @@ import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import { Box, Button, TextField, Typography, IconButton, InputAdornment } from '@mui/material';
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useState } from 'react';
+import React, { SyntheticEvent, useState } from 'react';
+import { axiosInstance } from '@/config';
 import Logo from '../../../../public/icon.png';
 import './style.css'; // Import plain CSS file
 
 function Login(): React.ReactElement {
   const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleClickShowPassword = (): void => {
     setShowPassword((prev) => !prev);
   };
+  const handleLogin = async (e: SyntheticEvent): Promise<void> => {
+    e.preventDefault();
+    await axiosInstance.post('/login', {
+      email,
+      password
+    });
+  };
 
   return (
-    <Box className="container">
+    <Box className="container" component="form" method="post" onSubmit={(e) => handleLogin(e)}>
       <Image src={Logo} alt="logo" className="logo" />
 
       <Box className="input-container">
-        <TextField size="small" placeholder="Email" fullWidth />
+        <TextField
+          size="small"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          fullWidth
+        />
         <Typography className="validation-message">Validation message</Typography>
       </Box>
 
@@ -28,6 +44,8 @@ function Login(): React.ReactElement {
         <TextField
           size="small"
           placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           type={showPassword ? 'text' : 'password'}
           fullWidth
           InputProps={{
@@ -51,7 +69,7 @@ function Login(): React.ReactElement {
         Forget Password
       </Link>
 
-      <Button variant="contained" color="primary" size="medium" className="button">
+      <Button variant="contained" color="primary" size="medium" className="button" type="submit">
         Login
       </Button>
 
