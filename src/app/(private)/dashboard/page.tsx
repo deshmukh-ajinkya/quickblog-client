75efd62f8ce0fb24c051db6c9b535641d9fef4ba
deshmukh-ajinkya <deshmukhasb@gmail.com>
2024-10-16 14:20:00 +0530

    feat: blog api init working

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -3,19 +3,46 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { Box, MenuItem, Select, SelectChangeEvent, Typography } from '@mui/material';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
-import { blogData } from '@/mock/blogData';
+import React, { useEffect, useState } from 'react';
+import { axiosInstance } from '@/config';
+import ReactIMG from '../../../../public/react.png';
 import Like from '../../../../public/thumbs-up.svg';
 import './style.css';
+
+interface Blog {
+  author: string;
+  title: string;
+  content: string;
+  image: string;
+  likesCount: number;
+}
 
 function Dashboard(): React.ReactNode {
   // State variable to store the selected category (initially set to 1)
   const [category, setCategory] = React.useState<number>(1);
-
+  const [blog, setBlog] = useState<Blog[]>([]);
   // Function to handle changes in the category selection dropdown
   const handleChange = (event: SelectChangeEvent): void => {
     setCategory(Number(event.target.value)); // Update state with the selected value converted to a number
   };
+
+  const handleGetBlog = async (): Promise<void> => {
+    const token = localStorage.getItem('token');
+    const { data } = await axiosInstance.post(
+      '/get_all_blog',
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    );
+    setBlog(data);
+  };
+
+  useEffect(() => {
+    handleGetBlog();
+  }, []);
 
   // Render the dashboard content
   return (
@@ -34,26 +61,27 @@ function Dashboard(): React.ReactNode {
       {/* Container for blog posts */}
       <Box className="dashboard-blog-container">
         {/* Loop through the first 10 blog posts from mock data */}
-        {blogData.slice(0, 10).map((blog, index) => (
+        {blog.map((blogData, index) => (
           <Link
             className="dashboard-blog-card-wrapper"
             key={index} // Set a unique key for each blog post
-            href={`/dashboard/${encodeURIComponent(blog.title.toLowerCase().replace(/\s+/g, '-'))}`} // Generate a dynamic URL based on blog title
+            href={`/dashboard/${encodeURIComponent(blogData.title.toLowerCase().replace(/\s+/g, '-'))}`} // Generate a dynamic URL based on blog title
           >
             {/* Blog post image */}
             <Image
-              src={blog.image}
+              src={ReactIMG}
               alt="Banner"
               width={400}
               height={200}
+              priority
               className="dashboard-blog-img"
             />
 
             {/* Blog post title */}
-            <Typography className="dashboard-blog-title">{blog.title}</Typography>
+            <Typography className="dashboard-blog-title">{blogData.title}</Typography>
 
             {/* Blog post description */}
-            <Typography className="dashboard-blog-description">{blog.description}</Typography>
+            <Typography className="dashboard-blog-description">{blogData.content}</Typography>
 
             {/* Container for blog post user information */}
             <Box className="dashboard-blog-user-info">
@@ -61,16 +89,24 @@ function Dashboard(): React.ReactNode {
               <AccountCircleIcon color="secondary" className="dashboard-user-info-icon" />
 
               {/* User name */}
-              <Typography className="dashboard-user-info-title">{blog.user.name}</Typography>
+              <Typography className="dashboard-user-info-title">
+                {blogData.author.toString()}
+              </Typography>
             </Box>
 
             {/* Container for blog post likes */}
             <Box className="dashboard-blog-user-like">
               {/* Number of likes */}
-              <Typography className="dashboard-user-info-title">{blog.user.likes}</Typography>
+              <Typography className="dashboard-user-info-title">{blogData.likesCount}</Typography>
 
               {/* Like icon */}
-              <Image src={Like} alt="like" width={20} className="dashboard-user-like-icon" />
+              <Image
+                priority
+                src={Like}
+                alt="like"
+                width={20}
+                className="dashboard-user-like-icon"
+              />
             </Box>
           </Link>
         ))}
